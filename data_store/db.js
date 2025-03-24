cb79627425e009d95acfdf6b5624073c71747b52
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-03-24 15:22:46 -0400

    Code fill-in

---
+++ data_store/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+// Create a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,  // Max number of connections in the pool
+  host: 'localhost',
+  user: 'yourUsername',
+  password: 'yourPassword',
+  database: 'yourDatabase'
+});
+
+// Export the pool
+module.exports = pool.promise(); // Use .promise() to enable async/await queries
