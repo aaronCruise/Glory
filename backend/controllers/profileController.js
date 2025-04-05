85476224a1659b01fa19857678beb110efef6cfd
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-04-05 15:48:22 -0400

    Syntax fix

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database');
+const db = require('../db');
 const path = require('path');
 
 function verifyDate(dateInput) {
