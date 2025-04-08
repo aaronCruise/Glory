4a49d76c4a172de17d49df7f4300386ce6f78f39
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-04-07 22:11:04 -0400

    profile

--- frontend/profile_user_logged_in/script.js
+++ frontend/profile_user_logged_in/script.js
@@ -1,40 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // Make sure the user is logged in before doing anything else on the page
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-        // Redirect to login page if the user is not logged in
-        console.log('No token found. User not logged in.');
-        window.location.replace('../login_without_user_logged_in');
-    }
-
-    fetch("http://128.6.60.9:8080/profile", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    })
-    .then(res => {
-        if (!res.ok) throw new Error("Unauthorized");
-        return res.json();
-    })
-    .then(data => {
-        document.getElementById("fullName").value = data.name;
-        document.getElementById("email").value = data.email;
-        document.getElementById("phone").value = data.phone;
-        document.getElementById("shipping-address").value = data.address;
-
-        if (data.profilePicUrl) {
-            document.getElementById("profile-pic").src = data.profilePicUrl;
-        }
-    })
-    .catch(err => {
-        console.log('Error fetching profile:', err);
-        window.location.replace('../login_without_user_logged_in');
-    })
-
-
     document.addEventListener('DOMContentLoaded', () => {
         const menu = document.querySelector("#mobile-menu")
         const menuLinks = document.querySelector(".navbar-menu")
