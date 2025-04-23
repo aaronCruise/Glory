f08441edbb09bf30ce9f5236eb617afde9bd9d13
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-04-23 18:46:06 -0400

    Fix

--- frontend/shared_elements/header.js
+++ frontend/shared_elements/header.js
@@ -1,5 +1,7 @@
 import {initHeader} from './header.js'
 fetch('./header_logged_in.html')
 .then(res => res.text())
-.then(data => {document.getElementById('header').innerHTML = data;});
-initHeader();
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+    initHeader();
+});
