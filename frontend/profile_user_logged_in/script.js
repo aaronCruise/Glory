9bcc8b2231d39c7228f407f5f8e00b1ae8dd3016
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-04-23 19:52:28 -0400

    Removed edit profile pic

--- frontend/profile_user_logged_in/script.js
+++ frontend/profile_user_logged_in/script.js
@@ -62,20 +62,20 @@ document.addEventListener('DOMContentLoaded', () => {
     	savebtn.style.display = 'none';
     });
 
-    // Edit Profile Picture Event
-    const changePicBtn = document.getElementById('change-pic-btn');
-    const profileLabel = document.getElementById('profile-pic-label');
-    const profileInput = document.getElementById('profile-pic-input');
+    // // Edit Profile Picture Event
+    // const changePicBtn = document.getElementById('change-pic-btn');
+    // const profileLabel = document.getElementById('profile-pic-label');
+    // const profileInput = document.getElementById('profile-pic-input');
 
-    changePicBtn.addEventListener('click', () =>{
-        profileLabel.style.display = 'inline-block';
-        changePicBtn.style.display = 'none';
+    // changePicBtn.addEventListener('click', () =>{
+    //     profileLabel.style.display = 'inline-block';
+    //     changePicBtn.style.display = 'none';
 
-        profileInput.addEventListener('change', () => {
-            if (profileInput.files.length > 0) {
-                profileLabel.style.display = 'none';
-                changePicBtn.style.display = 'inline-block';
-            }
-        });
-    });
+    //     profileInput.addEventListener('change', () => {
+    //         if (profileInput.files.length > 0) {
+    //             profileLabel.style.display = 'none';
+    //             changePicBtn.style.display = 'inline-block';
+    //         }
+    //     });
+    // });
 });
