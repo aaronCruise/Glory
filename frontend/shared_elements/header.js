f5aaf023a802af643180d34c1b804f494b63dbcf
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-04-23 18:48:23 -0400

    Path fix

--- frontend/shared_elements/header.js
+++ frontend/shared_elements/header.js
@@ -1,5 +1,5 @@
-import {initHeader} from './initialize_header.js'
-fetch('./header_logged_in.html')
+import {initHeader} from '../shared_elements/initialize_header.js'
+fetch('../shared_elements/header_logged_in.html')
 .then(res => res.text())
 .then(data => {
     document.getElementById('header').innerHTML = data;
