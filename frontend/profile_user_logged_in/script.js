330d4a3b7e15f60417d288b3908593d4948becd2
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-04-25 11:29:42 -0400

    Added logout button

--- frontend/profile_user_logged_in/script.js
+++ frontend/profile_user_logged_in/script.js
@@ -64,20 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
     	savebtn.style.display = 'none';
     });
 
-    // // Edit Profile Picture Event
-    // const changePicBtn = document.getElementById('change-pic-btn');
-    // const profileLabel = document.getElementById('profile-pic-label');
-    // const profileInput = document.getElementById('profile-pic-input');
-
-    // changePicBtn.addEventListener('click', () =>{
-    //     profileLabel.style.display = 'inline-block';
-    //     changePicBtn.style.display = 'none';
-
-    //     profileInput.addEventListener('change', () => {
-    //         if (profileInput.files.length > 0) {
-    //             profileLabel.style.display = 'none';
-    //             changePicBtn.style.display = 'inline-block';
-    //         }
-    //     });
-    // });
+    // Logout Event
+    
 });
