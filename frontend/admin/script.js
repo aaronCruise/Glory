e830630e91a76719bfa68fa354982bc25c2e46ac
lluis37 <129634078+lluis37@users.noreply.github.com>
2025-05-01 01:38:16 -0400

    Testing

--- frontend/admin/script.js
+++ frontend/admin/script.js
@@ -20,4 +20,41 @@ document.addEventListener("DOMContentLoaded", () => {
             preview.src = '';
         }
     });
+
+
+    const API_URL = '/products';
+    let allItems = [];
+
+    function categoryHTML(category) {
+        return `
+        <li class="filter-item" data-value="${category.trim().toLowerCase()}">
+        ${category.trim().charAt(0).toUpperCase() + category.trim().slice(1)}</li>
+        `
+    }
+
+    function addListCategories() {
+        const categories = [];
+        for (let i = 0; i < allItems.length; i++) {
+            let cat = allItems[i].category;
+            if(!categories.includes(cat)) {
+                categories.push(cat);
+            }
+        }
+        const list = document.querySelector(".filter-list");
+        const currentHTML = list.innerHTML;
+        list.innerHTML = currentHTML + categories.map(categoryHTML).join("");
+    }
+
+    async function intializeCategoryList() {
+        try {
+            const res = await fetch(API_URL);
+            const json = await res.json();
+            allItems = Array.isArray(json) ? json : json.items;
+            addListCategories();
+        } catch(err) {
+            console.log("Failed to load categories to be displayed:", err);
+        }
+    }
+
+    intializeCategoryList();
 });
